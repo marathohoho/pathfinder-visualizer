79c0228d783f8d9f26595740649877d31d18a4dc
marathohoho <codmarat@gmail.com>
2019-12-06 10:06:45 +0500

    created react app

---
+++ src/PathfinderVisualizer/PathfinderVisualizer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./PathfinderVisualizer.css";
+import Vertex from "./Vertex/Vertex";
+
+export default class PathfinderVisualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.props();
+  }
+  render() {
+    return (
+      <div>
+        Test
+        <Vertex>Test</Vertex>
+      </div>
+    );
+  }
+}
